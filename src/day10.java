8dc648b1b9b89ad34a6307fae9fe908a3448358f
nafeela <nafeelastar2405@gmail.com>
2026-06-16 15:15:28 +0530

    linkedlist

--- src/day10.java
+++ src/day10.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+/*import java.util.ArrayList;
 
 public class day10 {
     static void main(){
@@ -10,12 +10,33 @@ public class day10 {
         list.add("yalini");
         list.add("dharshini");
         for (String s:list){
-            System.out.print(s);
+            System.out.println(s);
         }
         list.remove("yalini");
         System.out.println();
         for(String s: list) {
-            System.out.print(s);
+            System.out.println(s);
         }
+    }
+
+}*/
+import java.util.LinkedList;
+
+public class day10 {
+    static void main() {
+        LinkedList<String> queue = new LinkedList<String>();
+        queue.add("gopi");
+        queue.add("yaz");
+        queue.add("pra");
+        queue.add("aish");
+        queue.add("dha");
+        while(!queue.isEmpty()) {
+            process(queue);
+        }
+    }
+    static void process(LinkedList<String> queue){
+        String removed=queue.removeFirst();
+        System.out.println("Removed:"+removed);
+
     }
 }
