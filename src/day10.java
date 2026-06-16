77e0aab7daa86a320b65932cb2c7a5628394900e
nafeela <nafeelastar2405@gmail.com>
2026-06-16 10:52:04 +0530

    arraylist

--- src/day10.java
+++ src/day10.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class day10 {
+    static void main(){
+        String[] students={"gopi","aish","prasanya","yalini","dharshini"};
+        ArrayList<String>list=new ArrayList<>();
+        list.add("gopi");
+        list.add("aish");
+        list.add("prasanya");
+        list.add("yalini");
+        list.add("dharshini");
+        for (String s:list){
+            System.out.print(s);
+        }
+        list.remove("yalini");
+        System.out.println();
+        for(String s: list) {
+            System.out.print(s);
+        }
+    }
 }
