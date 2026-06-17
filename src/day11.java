9cf96c3e261009ac38514c9f20e4441dd27feb87
nafeela <nafeelastar2405@gmail.com>
2026-06-17 16:13:06 +0530

    put

--- src/day11.java
+++ src/day11.java
@@ -1,4 +1,8 @@
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/*import java.util.HashSet;
 import java.util.Set;
 
 public class day11 {
@@ -15,4 +19,18 @@ public class day11 {
 
         }
     }
-}
+}*/
+import java.util.HashMap;
+import java.util.HashSet;
+         public class day11 {
+             public static void main(String[] args) {
+                 HashMap<String, Object> person = new HashMap<>();
+                 person.put("ram", "89");
+                 person.put("john", "78");
+                 System.out.println(person.get("percent"));
+                 person.entrySet().forEach(entry -> {
+                     System.out.println(entry.getKey() + " : " + entry.getValue());
+                 });
+             }
+         }
+
