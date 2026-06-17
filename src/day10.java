d6e2959ec801c4fb816f3922752b7dcd60182899
nafeela <nafeelastar2405@gmail.com>
2026-06-17 09:40:10 +0530

    link list 2

--- src/day10.java
+++ src/day10.java
@@ -17,9 +17,9 @@ public class day10 {
         for(String s: list) {
             System.out.println(s);
         }
-    }
+    }*/
 
-}*/
+/*
 import java.util.LinkedList;
 
 public class day10 {
@@ -38,5 +38,52 @@ public class day10 {
         String removed=queue.removeFirst();
         System.out.println("Removed:"+removed);
 
+    }
+}*/
+import java.util.LinkedList;
+class Student{
+    String name;
+    int regno;
+    int marks;
+    Student(String name ,int regno,int marks){
+        this.name=name;
+        this.regno=regno;
+        this.marks=marks;
+    }
+    @Override
+    public String toString(){
+        return "Name:"+name+
+                ",regno:"+regno+
+                ", Marks:"+marks;
+    }}
+public class day10{
+    static void main(){
+        LinkedList<Student>students=new LinkedList<>();
+        students.add(new Student("Aishu",101,85));
+        students.add(new Student("nafe",102,90));
+        students.add(new Student("gopi",103,87));
+        students.add(new Student("pra",104,78));
+        students.add(new Student("ravi",105,89));
+        System.out.println();
+        for(Student s:students){
+            System.out.println(s);
+        }
+        students.removeFirst();
+        System.out.println("\nAfter removing first student");
+        for(Student s:students){
+            System.out.println(s);
+        }
+        String searchvalue="Ravi";
+        for(Student s:students) {
+            if (s.name.equalsIgnoreCase(searchvalue)) {
+                s.marks = 95;
+                break;
+            }
+        }
+        System.out.println("\nAfter updating Ravi's marks:");
+        for(Student s:students){
+            System.out.println(s);
+
+        }
     }
 }
