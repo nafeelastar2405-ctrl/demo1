e6f599e37d7597fdad2ec2ae527c1af795bfe7f8
nafeela <nafeelastar2405@gmail.com>
2026-06-19 16:04:44 +0530

    files

--- src/day13.java
+++ src/day13.java
@@ -1,6 +1,11 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+
+/*import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +13,7 @@ public class day13 {
     static void main(){
         Path path = Path.of("files","report.txt");
         try {
-            List<String>data= Files.readAllLines(path);
+            List<String>data = Files.readAllLines(path);
             for (String line:data){
                 System.out.println(line);
             }
@@ -21,9 +26,29 @@ public class day13 {
             newData.add("Student 4");
             newData.add("Student 5");
         } catch (IOException io){
-            System.out.println("Error reading file");
+            io.printStackTrace();
         }
     }
 
+}
+*/
+public class day13 {
+    static void main(){
+        Path path= Path.of("files","student.csv");
+        try{
+            List<String>student= Files.readAllLines(path);
+            for (String Line :student){
+                String[]row=Line.split(",");
+                for (int i=0;i< row.length;i++){
+                    System.out.print(row[i]+" | ");
+
+                }
+                System.out.println();
+
+            }
+        }catch (IOException io){
+            io.printStackTrace();
+        }
+    }
 }
 
