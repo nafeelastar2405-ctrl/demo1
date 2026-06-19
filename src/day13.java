e58c939247782bf0138597f0bdbc7c70f08aa4ed
nafeela <nafeelastar2405@gmail.com>
2026-06-19 13:42:54 +0530

    filehandling

---
+++ src/day13.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class day13 {
+    static void main(){
+        Path path = Path.of("files","report.txt");
+        try {
+            List<String>data= Files.readAllLines(path);
+            for (String line:data){
+                System.out.println(line);
+            }
+            List<String>newData=new ArrayList<>();
+
+            newData.add("Student 1");
+            newData.add("Student 2");
+            newData.add("Student 3");
+            newData.add("Student 3");
+            newData.add("Student 4");
+            newData.add("Student 5");
+        } catch (IOException io){
+            System.out.println("Error reading file");
+        }
+    }
+
+}
+
