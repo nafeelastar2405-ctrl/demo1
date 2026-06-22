d193294792161c4740c3a94ad69d734ea62a66e5
nafeela <nafeelastar2405@gmail.com>
2026-06-22 14:52:01 +0530

    Email

--- src/day14_factory.java
+++ src/day14_factory.java
@@ -31,7 +31,7 @@
 
         }
 */
-public class day14_factory {
+/*public class day14_factory {
 
     interface Notification {
         void send(String to, String message);
@@ -66,5 +66,42 @@ public class day14_factory {
         String type = "Credit";
         getNotification(factory, type)
                 .send("240824", "Amount 800 Credited");
+    }
+}*/
+public class day14_factory {
+
+    interface Notification {
+        void send(String to, String message);
+    }
+
+    class EmailNotification implements Notification {
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Email: " + message + ", to " + to);
+        }
+    }
+
+    class WhatsappNotification implements Notification {
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Whatsapp: " + message + ", to " + to);
+        }
+    }
+
+    static Notification getNotification(day14_factory factory, String type) {
+        return switch (type) {
+            case "Email" -> factory.new EmailNotification();
+            case "Whatsapp" -> factory.new WhatsappNotification();
+            default -> throw new IllegalArgumentException(
+                    "Invalid notification type: " + type);
+        };
+    }
+
+    public static void main(String[] args) {
+        day14_factory factory = new day14_factory();
+
+        String type = "Whatsapp";
+        getNotification(factory, type)
+                .send("240824", "Hi Layaha");
     }
 }
